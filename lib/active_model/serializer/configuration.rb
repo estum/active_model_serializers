7a2f6bf2497dff9770d0eeaa2624e6e25cc2e4c8
Anton <anton.estum@gmail.com>
2015-08-20 06:21:50 +0300

    Add "use_sideloading" configuration parameter
     and a hook to apply it.

--- lib/active_model/serializer/configuration.rb
+++ lib/active_model/serializer/configuration.rb
@@ -7,6 +7,16 @@ module ActiveModel
       included do |base|
         base.config.array_serializer = ActiveModel::Serializer::ArraySerializer
         base.config.adapter = :flatten_json
+        base.config.use_sideloading = false
+
+        def self.configure
+          super
+
+          if config.use_sideloading
+            autoload :SideloadedAssociations
+            include SideloadedAssociations
+          end
+        end
       end
     end
   end
